ec9b07725577114bca800b2922af3d6883993124
Marcelo Chavez Caballero <132412188+Marcelo-xuy@users.noreply.github.com>
2023-05-29 13:39:00 -0400

    Add files via upload

---
+++ Tarea 11/code.js
@@ -0,0 +1,13 @@
+
+function ProNotas(){
+    var N = parseInt(prompt("Ingrese Notas a calcular "))
+    var S = 0
+    var i = 0
+    while ( N > i){
+        i = i + 1
+    var A = parseInt(prompt("Ingrese nota: "))
+        if (A >= 51){
+        S = S + A}
+} alert (S/N)
+
+}
